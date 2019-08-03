7732fb58eff676aa3bd54018bdc64af51e668485
wardDes <danielh9049@att.net>
2019-08-03 15:40:09 -0700

    init commit

---
+++ build/precache-manifest.297f8b83a4a97f1b49c83a3f12d3a537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2b38519c41e1318938a7ccc97c2fc5",
+    "url": "/wardDes/basic-react-tutorial/index.html"
+  },
+  {
+    "revision": "1bc3dc5d417b1a388fa7",
+    "url": "/wardDes/basic-react-tutorial/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "48f01e646d2317a937ee",
+    "url": "/wardDes/basic-react-tutorial/static/js/2.b390e18c.chunk.js"
+  },
+  {
+    "revision": "1bc3dc5d417b1a388fa7",
+    "url": "/wardDes/basic-react-tutorial/static/js/main.5305431b.chunk.js"
+  },
+  {
+    "revision": "291c65b556a178c9e6b3",
+    "url": "/wardDes/basic-react-tutorial/static/js/runtime~main.e2c201fc.js"
+  }
+]);
